0a3957b73ee89e464c53a762e356b9cee3598584
SumingYin <50822522+SumingYin@users.noreply.github.com>
2021-05-25 20:36:03 +0800

    Add files via upload

--- SelfGraph.java
+++ SelfGraph.java
@@ -1,11 +1,14 @@
 package com.ysm.offer;
 
+import org.junit.Test;
+
 import java.util.*;
 
 /**
  * 实现图的数据结构
  * 可赋予每个节点String名字
  */
+@SuppressWarnings("all")
 public class SelfGraph {
 
     // 哈希表存储所有节点
@@ -59,7 +62,7 @@ public class SelfGraph {
 
         Node node1 = selfGraph.get("1");
         Node node2 = selfGraph.get("2");
-        double node2len = selfGraph.get2NodesLength(node1,node2);
+        double node2len = selfGraph.get2NodesLength(node1, node2);
         System.out.println(node2len);
 
         // 2.逐个加入节点的方式，可在邻接矩阵的基础上，进行添加或删除节点、添加边
@@ -70,23 +73,51 @@ public class SelfGraph {
 
         // 添加边
         // 1.根据名称添加边
-        selfGraph1.addNextEdge("1","2",10);
-        selfGraph1.addNextEdge("3","2",8);
-        selfGraph1.addNextEdge("3","4",3);
-        selfGraph1.addNextEdge("5","6",1);
-        selfGraph1.addNextEdge("7","9",5);
+        selfGraph1.addNextEdge("1", "2", 10);
+        selfGraph1.addNextEdge("3", "2", 8);
+        selfGraph1.addNextEdge("3", "4", 3);
+        selfGraph1.addNextEdge("5", "6", 1);
+        selfGraph1.addNextEdge("7", "9", 5);
 
         // 2.根据名称数组添加边
         List<String> stringList = new ArrayList<>();
         ArrayList<Double> doubles = new ArrayList<>();
         for (int i = 1; i < 4; i++) {
-            stringList.add(i+"");
-            doubles.add((double)i);
+            stringList.add(i + "");
+            doubles.add((double) i);
         }
 
-        selfGraph1.addNextEdge("8",stringList,doubles);
+        selfGraph1.addNextEdge("8", stringList, doubles);
         System.out.println(selfGraph1);
 
+
+    }
+
+    @Test
+    public void test(){
+        // 测试最短路径算法
+        SelfGraph selfGraph = new SelfGraph();
+        double [][] Matrix = new double[6][6];
+        Matrix[0][1] = 5;
+        Matrix[0][2] = 3;
+        Matrix[1][2] = 2;
+        Matrix[1][3] = 6;
+        Matrix[2][3] = 7;
+        Matrix[2][4] = 4;
+        Matrix[2][5] = 10;
+        Matrix[3][4] = 1;
+        Matrix[3][5] = 1;
+        Matrix[4][5] = 2;
+        // 创建图
+        selfGraph.createDirectedGraph(6,Matrix);
+
+        // 测试应为无环
+        System.out.println(selfGraph.graphHasCircle());
+
+        // 测试最短路径
+        System.out.println(selfGraph.shortestPath2Nodes("2","5",selfGraph.graphHasCircle()));
+
+        System.out.println(selfGraph);
 
     }
 
@@ -101,7 +132,7 @@ public class SelfGraph {
         // 节点的个数
         nodeNums = n;
         // 长度不为n，或长度为0，长度不相等，均返回null
-        if (Matrix == null || Matrix.length != n ) {
+        if (Matrix == null || Matrix.length != n) {
             return null;
         } else if (Matrix[0] == null || Matrix.length != Matrix[0].length) {
             return null;
@@ -161,7 +192,7 @@ public class SelfGraph {
 
         nodeNums = n;
         // 长度不为n，或长度为0，长度不相等，均返回null，命名的长度与n不相等
-        if (Matrix == null || Matrix.length != n ) {
+        if (Matrix == null || Matrix.length != n) {
             return null;
         } else if (Matrix[0] == null || Matrix.length != Matrix[0].length) {
             return null;
@@ -265,15 +296,15 @@ public class SelfGraph {
         return addNextEdge(graphNodesMap.get(startName), graphNodesMap.get(endName), len);
     }
 
-    public boolean addNextEdge(String startName ,List<String> endNames,List<Double> lengths){
-        if(startName == null || endNames == null || lengths == null){
+    public boolean addNextEdge(String startName, List<String> endNames, List<Double> lengths) {
+        if (startName == null || endNames == null || lengths == null) {
             return false;
-        }else if(!graphNodesMap.containsKey(startName)){
+        } else if (!graphNodesMap.containsKey(startName)) {
             return false;
         }
         int size = endNames.size();
         int lenSize = lengths.size();
-        if(size != lenSize){
+        if (size != lenSize) {
             return false;
         }
 
@@ -316,7 +347,7 @@ public class SelfGraph {
         }
         int size = endNodes.size();
         int lenSize = lengths.size();
-        if(size != lenSize){
+        if (size != lenSize) {
             return false;
         }
         // 需要先检查所有添加的边是否符合条件
@@ -402,15 +433,16 @@ public class SelfGraph {
         return addNextEdge(endNode, list, list1);
 
     }
-    public boolean addPreEdge(String endName ,List<String> startNames,List<Double> lengths){
-        if(endName == null || startNames == null || lengths == null){
+
+    public boolean addPreEdge(String endName, List<String> startNames, List<Double> lengths) {
+        if (endName == null || startNames == null || lengths == null) {
             return false;
-        }else if(!graphNodesMap.containsKey(endName)){
+        } else if (!graphNodesMap.containsKey(endName)) {
             return false;
         }
         int size = startNames.size();
         int lenSize = lengths.size();
-        if(size != lenSize){
+        if (size != lenSize) {
             return false;
         }
 
@@ -510,7 +542,7 @@ public class SelfGraph {
     // 根据传入的两个节点名字的获取长度
     // 根据传入的两个节点的引用获取长度
     public double get2NodesLength(String startName, String endName) {
-        if(startName == null || endName == null || !graphNodesMap.containsKey(startName) || !graphNodesMap.containsKey(endName)){
+        if (startName == null || endName == null || !graphNodesMap.containsKey(startName) || !graphNodesMap.containsKey(endName)) {
             return -1.0;
         }
         return get2NodesLength(graphNodesMap.get(startName), graphNodesMap.get(endName));
@@ -525,7 +557,7 @@ public class SelfGraph {
 
         HashMap<Node, Edge> edgesMap = startNode.edgesMap;
         Edge edge = edgesMap.get(endNode);
-        if(edge == null){
+        if (edge == null) {
             // 说明两者之间不存在边
             return 0;
         }
@@ -533,17 +565,231 @@ public class SelfGraph {
 
     }
 
-    public double shortestPath2Nodes(Node startNode, Node endNode) {
+    // 根据名字进行深度遍历
+    public boolean depthFirstSearch(SelfGraph selfGraph, String name) {
+        if (name == null || name.length() == 0 || !selfGraph.graphNodesMap.containsKey(name)) {
+            return false;
+        }
 
-        // 首先判断是否可到达
-        // 如果可到达就计算最短的长度
-        return 1.0;
+        return depthFirstSearch(selfGraph, selfGraph.graphNodesMap.get(name));
+    }
+
+    public boolean depthFirstSearch(String name){
+        return depthFirstSearch(this,name);
+    }
+
+    // 重新设置所有节点是否被访问
+    public boolean reSetIsVisited(SelfGraph selfGraph) {
+        if (selfGraph == null || selfGraph.graphNodesMap == null) {
+            return false;
+        }
+        // 通过迭代器，首先将所有节点是否被访问过设置为false
+        Iterator<String> iterator = graphNodesMap.keySet().iterator();
+        while (iterator.hasNext()) {
+            Object next = iterator.next();
+            Node node1 = graphNodesMap.get(next);
+            node1.isVisited = false;
+        }
+        return true;
+    }
+
+    public boolean reSetIsVisited(){
+        return reSetIsVisited(this);
+    }
+
+
+
+    /**
+     * 深度遍历，递归进行遍历
+     * 在深度遍历之前应该reSetIsVisited
+     *
+     * @param selfGraph
+     * @param node
+     * @return
+     */
+    public boolean depthFirstSearch(SelfGraph selfGraph,Node node) {
+
+        HashMap<String, Node> graphNodesMap = selfGraph.graphNodesMap;
+        if (selfGraph != null || graphNodesMap == null || !graphNodesMap.containsValue(node)) {
+            return false;
+        }
+
+        // 从node开始进行深度遍历
+        node.isVisited = true;
+        List<Node> nextNodeList = node.nextNodeList;
+        if(nextNodeList == null){
+            return true;
+        }else {
+            for (Node n :
+                    nextNodeList) {
+                if(!n.isVisited){
+                    depthFirstSearch(selfGraph,n);
+                }
+            }
+        }
+
+
+        return true;
+    }
+
+    public boolean depthFirstSearch(Node node){
+        return depthFirstSearch(this,node);
+    }
+    // 广度遍历，借助队列来实现
+    public boolean breadthFirstSearch(SelfGraph selfGraph, Node node) {
+        return true;
+    }
+
+
+
+    /**
+     * 判断该图中是否存在以node为起点和终点的环
+     *
+     * @param startEndNode
+     * @return
+     */
+    public boolean nodeHasCircle(SelfGraph selfGraph,Node startEndNode) {
+        if(selfGraph == null){
+            return false;
+        }
+        HashMap<String, Node> graphNodesMap = selfGraph.graphNodesMap;
+        if( startEndNode == null || graphNodesMap == null || graphNodesMap.containsValue(startEndNode)){
+            return false;
+        }
+        // 重新设置是否访问
+        reSetIsVisited(selfGraph);
+        // 深度优先遍历
+        depthFirstSearch(selfGraph,startEndNode);
+        // 检查该节点的前驱节点是否存在isVisited为true的节点
+        // 如果存在，则一定存在
+        List<Node> preNodeList = startEndNode.preNodeList;
+        for (Node n :
+                preNodeList) {
+            if (n.isVisited){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean nodeHasCircle(Node startEndNode){
+        return nodeHasCircle(this,startEndNode);
+    }
+
+    /**
+     * 判断该图是否存在环
+     * @param selfGraph
+     * @return
+     */
+    public boolean graphHasCircle(SelfGraph selfGraph){
+
+        if(selfGraph == null){
+            return false;
+        }
+
+        HashMap<String, Node> graphNodesMap = selfGraph.graphNodesMap;
+        if(graphNodesMap == null || graphNodesMap.isEmpty()){
+            return false;
+        }
+        Set<String> strings = graphNodesMap.keySet();
+        for (String s:
+             strings) {
+            // 逐个节点判断，如果其中一个节点存在环，则该图存在环
+            if(nodeHasCircle(selfGraph,graphNodesMap.get(s))){
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+    public boolean graphHasCircle(){
+        return graphHasCircle(this);
+    }
+
+    // 返回两节点之间的最短距离,有向无环图
+    public double shortestPath2Nodes(String startName, String endName,boolean graphHasCircle) {
+
+        if (startName == null || endName == null || startName.length() == 0 || endName.length() == 0 || !graphNodesMap.containsKey(startName) || !graphNodesMap.containsKey(endName)) {
+            return -1.0;
+        }
+
+        return shortestPath2Nodes(graphNodesMap.get(startName), graphNodesMap.get(endName),graphHasCircle);
+    }
+
+    // 求解最短路径的时候涉及到浮点数的比较
+    final double error = 1e-6;
+
+    /**
+     * 先不考虑使用教科书的dijiska算法，之后再试一试
+     * 最直观采用动态规划的方法来计算两个点之间的最短距离
+     * 显然min(startNode,endNode) = min(len(startNode,nextNode) + min(nextNode,endNode)) 对任意的nextNode
+     * 由上式可得：可采用递归的方式来进行，期间需要添加一些判断语句
+     * 但该算法遇到存在环的有向图时会出现问题，所以需要先判断是否有环，其中graphHasCircle必须为false
+     * @param startNode
+     * @param endNode
+     * @return
+     */
+    public double shortestPath2Nodes(Node startNode, Node endNode,boolean graphHasCircle) {
+        // 两者指向相同则返回0
+        if (endNode == startNode) {
+            return 0.0;
+        }
+        List<Node> nextNodeList = startNode.nextNodeList;
+        // 无法到达最终节点时，返回-1.0
+        if (nextNodeList == null || nextNodeList.size() == 0 || graphHasCircle) {
+            return -1.0;
+        } else {
+
+            List<Double> minList  = new ArrayList<>();
+            for (Node nextNode :
+                    nextNodeList) {
+                double sWithn = get2NodesLength(startNode, nextNode);
+                if(nextNode == endNode){
+                    minList.add(sWithn);
+                    continue;
+                }
+                double nextMin = shortestPath2Nodes(nextNode,endNode,graphHasCircle);
+                if(Math.abs(nextMin+1.0) < error){
+                    // 说明nextNodeList为null无法到达endNode
+                    // 此时无需将该值加入进行比较
+                    continue;
+                }else {
+                    minList.add(sWithn + nextMin);
+                }
+
+            }
+            // 返回最小值，无法到达endNode
+            if(minList.size() == 0){
+                return -1.0;
+            }
+            return min(minList);
+
+        }
+
+    }
+
+    public Double min(List<Double> a) {
+        if (a == null || a.size() == 0) {
+            return null;
+        } else {
+            double min = a.get(0);
+            for (Double d :
+                    a) {
+                if (d < min) {
+                    min = d;
+                }
+            }
+            return min;
+        }
     }
 
 
     class Node {
 
         String name = null;
+        boolean isVisited = false;
 
         // 存储后继和前驱的所有节点
         // 和二叉树很像，这里nextNodelist是该节点指向的下一个节点集合
